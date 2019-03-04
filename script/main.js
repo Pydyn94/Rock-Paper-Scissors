ccf8bc4dbaac44cfcdc31126319ab903d8cb7c6a
Grzegorz Pydyn <g.pydyn@gmail.com>
2019-03-04 20:53:32 +0100

    Add variables on main.js

---
+++ script/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var rock = document.getElementById('rock');
+var paper = document.getElementById('paper');
+var scissors = document.getElementById('scissors');
+var output = document.getElementById('output');
